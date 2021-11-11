ab528babf8a6f04ccae0dc3932a6fe42951f5219
repleyva <rusvelpassos@gmail.com>
2021-11-11 09:56:02 -0500

    Agregando data binding

--- HartoEButi20/app/src/main/java/repleyva/dev/hartoebuti20/MainActivity.kt
+++ HartoEButi20/app/src/main/java/repleyva/dev/hartoebuti20/MainActivity.kt
@@ -2,10 +2,16 @@ package repleyva.dev.hartoebuti20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import repleyva.dev.hartoebuti20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // preparamos los datos de la actividad
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
